0883ccb7d89382dfe68b73936c2dfc1a24a3f7e8
John Verity <jverity@fatguymedia.com>
2020-01-02 08:22:44 -0500

    Fixes login redirect :: DEV-2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('welcome', 'welcome')->name('welcome');
 
 Route::post('login', 'Auth\LoginController@login')->name('login');
-Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
 
-Route::view('/', 'home')->middleware('auth');
+Route::view('/', 'home')->name('home')->middleware('auth');
