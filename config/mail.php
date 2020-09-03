569a963ce759e038111eb28df48d400a34056830
John Verity <jverity@irtsoftware.com>
2020-09-03 11:03:55 -0400

    email address change

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'support@bjjtrackr.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'john@bjjtrackr.com'),
         'name' => env('MAIL_FROM_NAME', 'BJJTrackr Support'),
     ],
 
