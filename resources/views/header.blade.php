03fde5154d45565f9ad6a15a503bfffb1f5857b5
John Verity <jverity@fatguymedia.com>
2021-07-08 10:46:08 -0400

    Implements analytics :: DEV-64

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -11,6 +11,16 @@
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
 
         <script type="application/javascript"> const CSRFToken = '{{ csrf_token() }}'; </script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-X21YE1QLR1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X21YE1QLR1');
+        </script>
     </head>
 
     <body style="background-color: #121212">
